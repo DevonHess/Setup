e3a50b2ef5e63bc65c4cf0c0c65327c3a18b7f4d
Devon Hess <DevonAlexanderHess@gmail.com>
2019-03-27 08:58:33 -0400

    name changes

---
+++ Tweak.bat
@@ -0,0 +1,38 @@
+@net session >nul 2>&1 || powershell -Command "Start-Process \"%0\" -Verb RunAs" && exit
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+CD /D "%~dp0\Registry\"
+
+FOR /F %%G IN ('DIR /B * ^| FIND /C /V ""') DO CHOICE /M "Install all "%%G" files?"
+SET all=%ERRORLEVEL%
+CLS
+
+FOR /R %%G IN (*) DO (
+	IF %all%==1 (
+		SET choice=1
+	) ELSE (
+		CALL :color "%%G"
+		CHOICE /M "Install "%%~nxG"?"
+		SET choice=!ERRORLEVEL!
+		CLS
+	)
+	IF !choice! EQU 1 (
+		IF %%~xG==.reg (
+			ECHO Installing %%~nxG
+			REG IMPORT "%%G"
+			ECHO:
+		) ELSE (
+			ECHO Running %%~nxG
+			CALL "%%G"
+			ECHO:
+		)
+	)
+)
+PAUSE
+EXIT
+
+:color
+ECHO [36m
+TYPE %1
+ECHO [0m
+EXIT /B
