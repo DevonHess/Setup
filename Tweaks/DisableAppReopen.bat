8dfda70a3111e3c9ce145e56740f328c6162c887
Devon Hess <DevonAlexanderHess@gmail.com>
2019-03-27 20:31:50 -0400

    fixed app reopen disable script

--- Tweaks/DisableAppReopen.bat
+++ Tweaks/DisableAppReopen.bat
@@ -1,5 +1,5 @@
-FOR /F "delims=" %%G IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserARSO"') DO (
-	ECHO Importing to %%~nG
-	REG ADD "%%G" /F /V OptOut /T REG_DWORD /D 00000001
+FOR /F %%G IN ('WMIC USERACCOUNT WHERE name^=^"%username%^" GET SID ^| FIND ^"S-^"') DO (
+	ECHO Importing to %%G
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserARSO\%%G" /F /V OptOut /T REG_DWORD /D 00000001
 )
 EXIT /B
