9164f9f7a9deb5dfce614effb7b7d6c2bcd272a6
Devon Hess <DevonAlexanderHess@gmail.com>
2019-03-30 18:03:27 -0400

    disable windows media player and explorer

--- Tweaks/DisableAppReopen.bat
+++ Tweaks/DisableAppReopen.bat
@@ -1,3 +1,5 @@
+@NET SESSION > NUL 2> NUL || PowerShell -Command "Start-Process \"%0\" -Verb RunAs" && EXIT
+@ECHO OFF
 FOR /F %%G IN ('WMIC USERACCOUNT WHERE name^=^"%username%^" GET SID ^| FIND ^"S-^"') DO (
 	ECHO Importing to %%G
 	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserARSO\%%G" /F /V OptOut /T REG_DWORD /D 00000001
---
+++ Tweaks/DisableWindowsMediaPlayer.bat
@@ -0,0 +1,3 @@
+@NET SESSION > NUL 2> NUL || PowerShell -Command "Start-Process \"%0\" -Verb RunAs" && EXIT
+@ECHO OFF
+DISM /Online /Disable-Feature /FeatureName:WindowsMediaPlayer /NoRestart
