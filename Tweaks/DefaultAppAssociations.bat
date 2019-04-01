91e4d342e6a05799a8bd9a6dfe207c2478552354
Devon Hess <DevonAlexanderHess@gmail.com>
2019-03-31 21:07:19 -0400

    removed failed attempt at automatic file associations

--- Tweaks/DefaultAppAssociations.bat
+++
@@ -1,6 +0,0 @@
-@NET SESSION > NUL 2> NUL || PowerShell -Command "Start-Process \"%0\" -Verb RunAs" && EXIT
-@ECHO OFF
-CD /D "%~dp0\..\Extra\"
-COPY "defaultassociations.xml" "c:\windows\system32\defaultassociations.xml"
-DISM /online /Import-DefaultAppAssociations:"..\Extra\DefaultAppAssociations.xml"
-EXIT /B
